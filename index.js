5327326c88b3109b9d970c28ed8eb765db7a0328
Shiananmiad Bermudez <bermudezdamian7@gmail.com>
2021-08-14 17:01:12 -0300

    enviar formulario y agregue iconos imagenes

--- index.js
+++ index.js
@@ -132,9 +132,29 @@ function SliderClientes() {
   }, 5000);
 }
 
+function enviarFormulario() {
+  const $form = document.querySelector("#form");
+  const $buttonEnviar = document.querySelector("#enviar-mail");
+  $form.addEventListener("submit", handleSubmit);
+  function handleSubmit(event) {
+    event.preventDefault();
+    const form = new FormData(this);
+    $buttonEnviar.setAttribute(
+      "href",
+      `mailto:bermudezdamian7@gmail.com?subject= Nombre:${form.get(
+        "name"
+      )} Email:${form.get("mail")} Telefono:${form.get(
+        "phone"
+      )} Asunto:${form.get("asunto")}&body=${form.get("message")} `
+    );
+    $buttonEnviar.click();
+  }
+}
+
 //animacion de
 
 function main() {
+  enviarFormulario();
   SliderClientes();
   animacionScroolIzquierda();
   menuActive();
